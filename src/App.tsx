b5c336a44c677882b1cf2f5021d497d04864f29a
Argon <sgs6tomekxd@gmail.com>
2025-10-24 16:58:07 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -209,6 +209,16 @@ const App: React.FC = () => {
               </a>
               </div>
           </div>
+          {/* Wskazówka do przewijania */}
+          <div 
+            className="absolute bottom-8 left-1/2 -translate-x-1/2 z-10 animate-bounce-subtle text-gray-400 cursor-pointer transition-colors hover:text-white"
+            onClick={() => window.scrollTo({ top: window.innerHeight, behavior: 'smooth' })}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
+            </svg>
+            <span className="sr-only">Przewiń w dół</span>
+          </div>
         </header>
         
         {/* Główna Zawartość Strony */}
